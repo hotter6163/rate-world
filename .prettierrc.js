951aa999382c3619acf4726820f8900c0954f8e3
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-06-17 15:43:53 +0900

    :art: introduce prettier and run "yarn format"

---
+++ .prettierrc.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [require('prettier-plugin-tailwindcss')],
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 100,
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+};
