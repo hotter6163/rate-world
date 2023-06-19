43d1d0ddf940cd8a34e58180d4f1c00a08a605b7
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-06-19 21:54:00 +0900

    :art: add liter for graphql

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
-  extends: ["next/core-web-vitals", "prettier"],
+  extends: ['next/core-web-vitals', 'prettier'],
+  overrides: [
+    {
+      files: ['*.js'],
+      processor: '@graphql-eslint/graphql',
+    },
+    {
+      files: ['*.graphql'],
+      parser: '@graphql-eslint/eslint-plugin',
+      plugins: ['@graphql-eslint'],
+      rules: {
+        '@graphql-eslint/known-type-names': 'error',
+      },
+    },
+  ],
 };
