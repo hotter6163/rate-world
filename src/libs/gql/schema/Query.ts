82d1afb08732a07515e287b93f728f0a18e80a28
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-06-24 16:21:32 +0900

    :adhesive_bandage: fix file path

--- src/libs/gql/schema/Query.ts
+++ src/libs/gql/schema/Query.ts
@@ -1,4 +1,4 @@
-import { userQuery } from '../../user/resolvers/Query/user';
+import { userQuery } from './user/resolvers/query/user';
 import { QueryResolvers } from '@/libs/gql/generated/resolvers-types';
 
 export const Query: QueryResolvers = {
