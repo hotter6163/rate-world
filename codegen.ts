69156c337af6f02da0a4e936e07a902f85a27ad3
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-06-25 10:31:32 +0900

    Revert ":fire: remove unnecessary config"
    This reverts commit 702c6edfc0cdf8ab6927fbbc9621525dbc8930c8.

--- codegen.ts
+++ codegen.ts
@@ -1,6 +1,7 @@
 import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
+  overwrite: true,
   schema: './src/libs/gql/schema/**/*.graphql',
   documents: ['src/**/*.tsx', 'src/**/*.ts'],
   ignoreNoDocuments: true,
