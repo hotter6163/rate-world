07b385b59bf848cf8a972194fdede057389c4484
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-01 14:53:36 +0900

    :fire: remove unnecessary code

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,6 +1,4 @@
 import { getServerSession } from '@/libs/auth/common/getServerSession';
-import { apolloClient } from '@/libs/gql/client';
-import { graphql } from '@/libs/gql/generated';
 import { FC, ReactNode } from 'react';
 
 interface Props {
@@ -8,24 +6,8 @@ interface Props {
   unauthenticated: ReactNode;
 }
 
-const ExampleQueryQuery = graphql(/* GraphQL */ `
-  query ExampleQuery($id: String!) {
-    user(id: $id) {
-      id
-      name
-      age
-    }
-  }
-`);
-
 const HomeLayout: FC<Props> = async ({ authenticated, unauthenticated }) => {
   const session = await getServerSession();
-  const { data, error } = await apolloClient.query({
-    query: ExampleQueryQuery,
-    variables: {
-      id: '1',
-    },
-  });
 
   return <>{session ? authenticated : unauthenticated}</>;
 };
