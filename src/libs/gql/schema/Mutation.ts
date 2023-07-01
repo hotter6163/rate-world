77bbdc898b830b2d09203c630e8b59a865395c47
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-01 15:04:13 +0900

    Revert ":green_heart: fix build error"
    This reverts commit f5c97e6d2800a022e70e4c5f5eb8036488a92c8b.

--- src/libs/gql/schema/Mutation.ts
+++ src/libs/gql/schema/Mutation.ts
@@ -1,5 +1,5 @@
+import { createUserMutation } from './user/resolvers/mutation/createUser';
 import { MutationResolvers } from '@/libs/gql/generated/resolvers-types';
-import { createUserMutation } from '@/libs/gql/schema/user/resolvers/mutation/createUser';
 
 export const Mutation: MutationResolvers = {
   createUser: createUserMutation,
