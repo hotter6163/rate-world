3f90a025c4e1a8067a9bb862f1eec915cb1720df
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-02 09:24:36 +0900

    :speech_balloon: add query to fix error

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,10 +1,19 @@
 import { getServerSession } from '@/libs/auth/common/getServerSession';
+import { graphql } from '@/libs/gql/generated';
 import { FC, ReactNode } from 'react';
 
 interface Props {
   authenticated: ReactNode;
   unauthenticated: ReactNode;
 }
+
+const query = graphql(`
+  query TestQuery($id: ID!) {
+    user(id: $id) {
+      id
+    }
+  }
+`);
 
 const HomeLayout: FC<Props> = async ({ authenticated, unauthenticated }) => {
   const session = await getServerSession();
