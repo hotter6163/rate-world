f86f413a692e19d5c97601450b0c540804c2102a
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-02 11:06:50 +0900

    :fire: remove config. avoidOptionals

--- codegen.ts
+++ codegen.ts
@@ -15,7 +15,6 @@ const config: CodegenConfig = {
     },
     './src/libs/gql/generated/resolvers-types.ts': {
       config: {
-        avoidOptionals: true,
         contextType: '@/libs/gql/server/context#Context',
         mapperTypeSuffix: 'Model',
         mappers: {
