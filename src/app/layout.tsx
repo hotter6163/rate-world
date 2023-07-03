12276a80ff1e22bb863dcc43fd7fbc5b455e3b32
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-03 10:34:58 +0900

    :mag: set default meatdata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,19 @@
 import './globals.css';
 import { SessionProvider, getServerSession } from '@/libs/auth';
+import { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import { FC, ReactNode } from 'react';
 
 const inter = Inter({ subsets: ['latin'] });
 
-export const metadata = {
-  title: 'Rate World',
+export const metadata: Metadata = {
+  title: {
+    template: '%s - Rate World（仮）',
+    absolute: 'Rate World（仮）',
+  },
   description:
     'レートシステムを使った対戦ゲームができます。なんかちゃんと作り切ること目標。一旦ゲームとレートシステムは適当。since 2023/06/18',
+  viewport: 'width=device-width, initial-scale=1, maximum-scale=1.0',
 };
 
 interface Props {
