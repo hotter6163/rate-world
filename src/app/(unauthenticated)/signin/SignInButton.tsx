f8240eb2e7187b4f204957415ded36ab5e86d19c
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-03 10:35:52 +0900

    :lipstick: update signin page

--- src/app/(unauthenticated)/signin/SignInButton.tsx
+++ src/app/(unauthenticated)/signin/SignInButton.tsx
@@ -4,7 +4,7 @@ import { signInWithLine } from '@/libs/auth';
 import { FC } from 'react';
 
 export const SignInButton: FC = () => (
-  <button onClick={() => signInWithLine()} className="rounded-lg bg-green-500 px-4 py-2 text-white">
-    sign in with line
+  <button onClick={() => signInWithLine()} className="rounded-xl bg-green-500 px-4 py-2 text-white">
+    LINEでログイン
   </button>
 );
--- src/app/(unauthenticated)/signin/page.tsx
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -1,6 +1,23 @@
 import { SignInButton } from './SignInButton';
+import { Logo } from '@/components/layout/Logo';
+import { Metadata } from 'next';
+import Link from 'next/link';
 import { FC } from 'react';
 
-const SignInPage: FC = () => <SignInButton />;
+export const metadata: Metadata = {
+  title: 'ログイン',
+};
+
+const SignInPage: FC = () => (
+  <div className="flex h-screen w-full items-center justify-center">
+    <section className="flex flex-col items-center gap-6 rounded-xl px-8 py-6 shadow-pop">
+      <Logo />
+      <SignInButton />
+      <Link href="/">
+        <p className="text-sm font-normal underline">ホームに戻る</p>
+      </Link>
+    </section>
+  </div>
+);
 
 export default SignInPage;
