6f9ea129ae24efe3b794b70c32836e0690cc68b8
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-04 08:47:17 +0900

    :bug: fix that prisma client can't call from client

--- src/libs/prisma/index.ts
+++ src/libs/prisma/index.ts
@@ -1,3 +1,10 @@
 import { PrismaClient } from '@prisma/client';
 
-export const prismaClient = new PrismaClient();
+const globalForPrisma = global as unknown as { prisma: PrismaClient };
+
+export const prisma =
+  globalForPrisma.prisma || (typeof window === 'undefined' ? new PrismaClient() : undefined);
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+
+export default prisma;
