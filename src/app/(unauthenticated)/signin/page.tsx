e1f29523222ac2722458a60981c81f3601dffe3a
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-04 09:30:44 +0900

    :bento: add sign in form

--- src/app/(unauthenticated)/signin/page.tsx
+++ src/app/(unauthenticated)/signin/page.tsx
@@ -1,7 +1,5 @@
-import { SignInButton } from './SignInButton';
-import { Logo } from '@/components/layout/Logo';
+import { SignInForm } from '@/components/auth/SigninForm';
 import { Metadata } from 'next';
-import Link from 'next/link';
 import { FC } from 'react';
 
 export const metadata: Metadata = {
@@ -10,13 +8,7 @@ export const metadata: Metadata = {
 
 const SignInPage: FC = () => (
   <div className="flex h-screen w-full items-center justify-center">
-    <section className="flex flex-col items-center gap-6 rounded-xl px-8 py-6 shadow-pop">
-      <Logo />
-      <SignInButton />
-      <Link href="/">
-        <p className="text-sm font-normal underline">ホームに戻る</p>
-      </Link>
-    </section>
+    <SignInForm />
   </div>
 );
 
