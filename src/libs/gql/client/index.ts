55f0ea48264f49cdfa03f985ff6cc9e86e16c564
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-09 16:36:18 +0900

    :adhesive_bandage: change env name

--- src/libs/gql/client/index.ts
+++ src/libs/gql/client/index.ts
@@ -2,6 +2,6 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const apolloClient = new ApolloClient({
   ssrMode: true,
-  uri: process.env.NEXT_PUBLIC_API_URI,
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URI,
   cache: new InMemoryCache(),
 });
