fa013ad1e15994540b3b697628786e5545bfee08
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-12 11:38:10 +0900

    :bug: fix type error

--- src/libs/gql/server/index.ts
+++ src/libs/gql/server/index.ts
@@ -3,13 +3,13 @@ import { Context, context } from './context';
 import { resolvers } from './resolvers';
 import { ApolloServer } from '@apollo/server';
 import { startServerAndCreateNextHandler } from '@as-integrations/next';
-import { NextApiRequest } from 'next';
+import { NextRequest } from 'next/server';
 
 const server = new ApolloServer<Context>({
   resolvers,
   typeDefs,
 });
 
-export const apolloServer = startServerAndCreateNextHandler<NextApiRequest, Context>(server, {
+export const apolloServer = startServerAndCreateNextHandler<NextRequest, Context>(server, {
   context,
 });
