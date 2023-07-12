ef6bdce93d9b246e6a81ada90677df1a85dccea2
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-12 10:09:45 +0900

    :coffin: remove createUser mutation

--- src/libs/gql/schema/Mutation.ts
+++ src/libs/gql/schema/Mutation.ts
@@ -1,6 +1,4 @@
-import { createUserMutation } from './user';
 import { MutationResolvers } from '@/libs/gql/generated/resolvers-types';
 
 export const Mutation: MutationResolvers = {
-  createUser: createUserMutation,
 };
--- src/libs/gql/schema/user/index.ts
+++ src/libs/gql/schema/user/index.ts
@@ -1,4 +1,2 @@
-export * from './resolvers/mutation/createUser';
-
 export * from './resolvers/query/user';
 export * from './resolvers/query/users';
