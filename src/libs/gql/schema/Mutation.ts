2ecda4a74cefcb0965964de745a3ed199fdf3285
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-13 22:23:56 +0900

    :sparkles: add authorize channel mutation

--- src/libs/gql/schema/Mutation.ts
+++ src/libs/gql/schema/Mutation.ts
@@ -1,6 +1,8 @@
 import { authenticateUserMutation } from './pusher';
+import { authorizeChannelMutation } from './pusher/resolvers/mutation/authorizeChannel';
 import { MutationResolvers } from '@/libs/gql/generated/resolvers-types';
 
 export const Mutation: MutationResolvers = {
   authenticateUser: authenticateUserMutation,
+  authorizeChannel: authorizeChannelMutation,
 };
