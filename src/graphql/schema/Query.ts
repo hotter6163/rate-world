93c35a0a6396ef0eaab1b5cda3750a1e69091731
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-20 19:00:23 +0900

    :wastebasket: add query to check kv value

--- src/graphql/schema/Query.ts
+++ src/graphql/schema/Query.ts
@@ -1,7 +1,15 @@
 import { userQuery, usersQuery } from './user';
-import { QueryResolvers } from '@/graphql/generated/resolvers-types';
+import { Game } from '@/games/Game';
+import { QueryResolvers, UserData } from '@/graphql/generated/resolvers-types';
 
 export const Query: QueryResolvers = {
   user: userQuery,
   users: usersQuery,
+  test: async (_, __, { kv, pusher }) => {
+    const data = await kv.lrange<UserData>(Game.BattleLine, 0, -1);
+    const res = await pusher.sendToUser('cljjjz5c00000y1kowu2zcbl2', 'matched', {
+      roomId: 'roomId',
+    });
+    return data;
+  },
 };
