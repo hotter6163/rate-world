068aa12c768673f6c1442d0122bf7e27fb455eb5
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-22 17:22:58 +0900

    :necktie: connect matching channel if matching success

---
+++ src/features/socket/hooks/useMatching.ts
@@ -0,0 +1,74 @@
+'use client';
+
+import { matchingMatchedHandler } from '../handlers/matching/matched';
+import { usePusher } from './usePusher';
+import { useSubscribe } from './useSubscribe';
+import { useUnsubscribe } from './useUnsubscribe';
+import { graphql } from '@/graphql/generated';
+import { splitChannelName } from '@/libs/pusher';
+import { useMutation } from '@apollo/client';
+import { useEffect } from 'react';
+
+const mutation = graphql(`
+  mutation CreateMatching($input: CreateMatchingInput!) {
+    createMatching(input: $input) {
+      result {
+        type
+        ... on MatchingSuccess {
+          roomId
+        }
+      }
+    }
+  }
+`);
+
+export const useMatching = () => {
+  const { pusher, channel } = usePusher();
+  const { subscribe } = useSubscribe();
+  const { unsubscribe } = useUnsubscribe();
+  const [matching] = useMutation(mutation, {
+    fetchPolicy: 'no-cache',
+  });
+
+  useEffect(() => {
+    if (!pusher) return;
+    if (!channel) return;
+    const { id } = splitChannelName(channel.name);
+    if (id !== 'matching') return;
+
+    const callback = async () => {
+      const { data } = await matching({
+        variables: {
+          input: {
+            channelName: channel.name,
+          },
+        },
+      });
+
+      switch (data?.createMatching.result?.__typename) {
+        case 'MatchingSuccess':
+          console.log('MatchingSuccess', data.createMatching.result.roomId);
+          subscribe(data.createMatching.result.roomId, true);
+          break;
+        case 'MatchingRetry':
+          break;
+        case 'MatchingTimeout':
+          unsubscribe();
+          break;
+      }
+    };
+    const interval = setInterval(callback, 1000 * 10);
+
+    const matchedHandler = matchingMatchedHandler((roomId) => {
+      console.info(`Matched: ${roomId}`);
+      subscribe(roomId, true);
+      clearInterval(interval);
+    });
+    pusher.user.bind('matched', matchedHandler);
+
+    return () => {
+      pusher.user.unbind('matched', matchedHandler);
+      clearInterval(interval);
+    };
+  }, [pusher, channel]);
+};
--- src/features/socket/index.ts
+++ src/features/socket/index.ts
@@ -5,6 +5,7 @@ export * from './const/PusherContext';
 export * from './hooks/useCallEvent';
 export * from './hooks/useConnect';
 export * from './hooks/useDisconnect';
+export * from './hooks/useMatching';
 export * from './hooks/usePusher';
 export * from './hooks/useSubscribe';
 export * from './hooks/useUnsubscribe';
