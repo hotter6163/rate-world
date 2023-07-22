7707b4e546bda2b2ddb6685e92c6bc0bc3d9ea79
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-22 18:28:17 +0900

    :zap: reduce time to delete values stored in kv

--- src/graphql/schema/pusher/resolvers/mutation/createMatching.ts
+++ src/graphql/schema/pusher/resolvers/mutation/createMatching.ts
@@ -1,12 +1,13 @@
 import { CreateMatchingResultType, MutationResolvers } from '@/graphql/generated/resolvers-types';
 import { BattleLineStoreItem } from '@/libs/kv';
 import { getChannelName, splitChannelName } from '@/libs/pusher';
+import { ArenaStatus, Game } from '@prisma/client';
 import dayjs from 'dayjs';
 
 export const createMatchingMutation: MutationResolvers['createMatching'] = async (
   _,
   { input: { channelName } },
-  { token, dataSources: { session }, kv, pusher },
+  { token, dataSources: { session, arena }, kv, pusher },
 ) => {
   if (!token) return { result: null, status: 400 };
   const channel = splitChannelName(channelName);
@@ -18,6 +19,7 @@ export const createMatchingMutation: MutationResolvers['createMatching'] = async
       .findUnique({ select: { userId: true }, where: { sessionToken: token } })
       .then((session) => session?.userId),
   ]);
+
   if (!userId) return { result: null, status: 400 };
   const user = matchingData.find((item) => item.user.id === userId);
   if (!user) return { result: null, status: 400 };
@@ -36,12 +38,47 @@ export const createMatchingMutation: MutationResolvers['createMatching'] = async
   }
 
   const opponent = opponents[Math.floor(Math.random() * opponents.length)];
-  const roomId = getChannelName({ prefix: 'presence', game: channel.game, id: 'roomId' });
-  await Promise.all([
+  const arenaId = await Promise.allSettled([
+    arena
+      .create({
+        data: {
+          game: Game.BATTLE_LINE,
+          status: ArenaStatus.MATCHING,
+          participants: {
+            create: [
+              {
+                participantId: user.user.id,
+                matchRating: user.rate,
+              },
+              {
+                participantId: opponent.user.id,
+                matchRating: opponent.rate,
+              },
+            ],
+          },
+        },
+      })
+      .then((result) => result.id),
     kv.lrem(channel.game, 0, user),
     kv.lrem(channel.game, 0, opponent),
-    pusher.sendToUser(opponent.user.id, 'matched', { roomId }),
-  ]);
+  ]).then((result) => {
+    if (result[0].status === 'rejected') {
+      return Promise.all([
+        new Promise((resolve) =>
+          result[1].status === 'fulfilled' ? kv.lpush(channel.game, user) : resolve(-1),
+        ),
+        new Promise((resolve) =>
+          result[2].status === 'fulfilled' ? kv.lpush(channel.game, opponent) : resolve(-1),
+        ),
+      ]).then(() => null);
+    } else {
+      return result[0].value;
+    }
+  });
+
+  if (!arenaId) return { result: null, status: 400 };
+  const roomId = getChannelName({ prefix: 'presence', game: channel.game, id: arenaId });
+  await pusher.sendToUser(opponent.user.id, 'matched', { roomId });
   return {
     result: { type: CreateMatchingResultType.Success, roomId },
     status: 200,
