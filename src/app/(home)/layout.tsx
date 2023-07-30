5b7b3be8c2d7a1358451f7f968317cb8e71bda9b
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-07-30 09:07:49 +0900

    :fire: remove unnecessary query

--- src/app/(home)/layout.tsx
+++ src/app/(home)/layout.tsx
@@ -1,20 +1,11 @@
 import { Header } from '@/components/layout/Header';
 import { getServerSession } from '@/features/auth/utils/getServerSession';
-import { graphql } from '@/graphql/generated';
 import { FC, ReactNode } from 'react';
 
 interface Props {
   authenticated: ReactNode;
   unauthenticated: ReactNode;
 }
-
-const query = graphql(`
-  query TestQuery($id: ID!) {
-    user(id: $id) {
-      id
-    }
-  }
-`);
 
 const HomeLayout: FC<Props> = async ({ authenticated, unauthenticated }) => {
   const session = await getServerSession();
