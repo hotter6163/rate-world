9aaeb3796406d2f466bb36ed4e6324fbd0703bd0
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-09 09:57:05 +0900

    :test_tube: add battle line test

---
+++ __tests__/battleLine/store.test.ts
@@ -0,0 +1,15 @@
+import { unitStack } from '@/features/games/battleLine/constants';
+import { useBattleLineStore } from '@/features/games/battleLine/store';
+import { act, renderHook } from '@testing-library/react';
+
+describe('battleLine/store', () => {
+  it('ストアの初期化', () => {
+    const { result } = renderHook(() => useBattleLineStore());
+    console.log(unitStack.length);
+    act(() => result.current.setup());
+    expect(result.current.unitStack.length).toBe(unitStack.length - 14);
+    expect(result.current.tacticalStack.length).toBe(10);
+    expect(result.current.myHands.length).toBe(7);
+    expect(result.current.opponentHands.length).toBe(7);
+  });
+});
