c6b1d1b02957fefa8a61f45f7bda49236b6fa2b7
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-09 21:08:07 +0900

    :recycle: update battle line store test

--- __tests__/battleLine/store.test.ts
+++ __tests__/battleLine/store.test.ts
@@ -1,14 +1,30 @@
-import { unitStack } from '@/features/games/battleLine/constants';
-import { useBattleLineStore } from '@/features/games/battleLine/store';
-import { act, renderHook } from '@testing-library/react';
+import { tacticalStack, unitStack } from '@/features/games/battleLine/constants';
+import { BattleLineStore, useBattleLineStore } from '@/features/games/battleLine/store';
+import { RenderHookResult, act, renderHook } from '@testing-library/react';
 
 describe('battleLine/store', () => {
-  it('ストアの初期化', () => {
-    const { result } = renderHook(() => useBattleLineStore());
-    act(() => result.current.setup());
-    expect(result.current.unitStack.length).toBe(unitStack.length - 14);
-    expect(result.current.tacticalStack.length).toBe(10);
-    expect(result.current.myHands.length).toBe(7);
-    expect(result.current.opponentHands.length).toBe(7);
+  let storeHookResult: RenderHookResult<BattleLineStore, unknown>;
+  let result: { current: BattleLineStore };
+
+  beforeEach(() => {
+    storeHookResult = renderHook(() => useBattleLineStore());
+    result = storeHookResult.result;
+  });
+
+  describe('setup', () => {
+    it('ストアの初期化前は、unitStack、tacticalStack、myHands、opponentHandsが空である', () => {
+      expect(result.current.unitStack.length).toBe(0);
+      expect(result.current.tacticalStack.length).toBe(0);
+      expect(result.current.myHands.length).toBe(0);
+      expect(result.current.opponentHands.length).toBe(0);
+    });
+
+    it('ストアの初期化', () => {
+      act(() => result.current.setup());
+      expect(result.current.unitStack.length).toBe(unitStack.length - 14);
+      expect(result.current.tacticalStack.length).toBe(tacticalStack.length);
+      expect(result.current.myHands.length).toBe(7);
+      expect(result.current.opponentHands.length).toBe(7);
+    });
   });
 });
