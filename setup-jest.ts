e50560eeeaf5a3d14fc4e65020cbddb734cbaadf
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-11 13:59:04 +0900

    :goal_net: ignore build error

--- setup-jest.ts
+++ setup-jest.ts
@@ -1,1 +1,11 @@
 import '@testing-library/jest-dom';
+
+const ignoredErrors = [
+  /act(...) is not supported in production builds of React, and might not behave as expected./,
+];
+const consoleError = global.console.error;
+global.console.error = (...args) => {
+  if (!ignoredErrors.find((el) => el.test(args[0]))) {
+    return consoleError(...args);
+  }
+};
