6f197912edaea51ad0ac22a2ef3c315f8d96b581
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-11 13:45:26 +0900

    :bento: can play card without 謀略戦術カード

--- __tests__/battleLine/store.test.ts
+++ __tests__/battleLine/store.test.ts
@@ -1,6 +1,6 @@
 import { tacticalStack, unitStack } from '@/features/games/battleLine/constants';
 import { BattleLineStore, useBattleLineStore } from '@/features/games/battleLine/store';
-import { Card } from '@/features/games/battleLine/types';
+import { Card, TacticalType } from '@/features/games/battleLine/types';
 import { RenderHookResult, act, renderHook } from '@testing-library/react';
 
 describe('battleLine/store', () => {
@@ -46,7 +46,7 @@ describe('battleLine/store', () => {
           });
       });
 
-      it('境界テスト', () => {
+      it('存在しないカードを選択するとエラー', () => {
         const test = (index: number) => () => act(() => result.current.selectHand(index));
         expect(test(-1)).toThrowError();
         expect(test(7)).toThrowError();
@@ -54,17 +54,126 @@ describe('battleLine/store', () => {
     });
 
     describe('自分の手札を場に出す', () => {
-      it('通常操作', () => {
-        const battlefieldIndex = Math.floor(Math.random() * 9);
-        const cardIndex = Math.floor(Math.random() * 7);
-        const card = { ...result.current.myHands[cardIndex] };
+      describe('ユニットカード', () => {
+        it('カードを場に出す', () => {
+          Array(9)
+            .fill(null)
+            .forEach((_, index) => {
+              const cardIndex = Math.floor(Math.random() * 7);
+              const card = { ...result.current.myHands[cardIndex] };
+              expect(result.current.battlefields[index].myFormation.length).toBe(0);
+              act(() => result.current.selectHand(cardIndex));
+              act(() => result.current.playCard(index));
+              expect(result.current.battlefields[index].myFormation.length).toBe(1);
+              expect(result.current.battlefields[index].myFormation[0]).toEqual(card);
+              expect(result.current.myHands.length).toBe(6);
+              expect(result.current.myHands).not.toContainEqual(card);
+              act(() => result.current.drawCard('UNIT'));
+            });
+        });
 
-        expect(result.current.battlefields[battlefieldIndex].myFormation.length).toBe(0);
-        act(() => result.current.selectHand(cardIndex));
-        act(() => result.current.playCard(battlefieldIndex));
-        expect(result.current.battlefields[battlefieldIndex].myFormation.length).toBe(1);
-        expect(result.current.battlefields[battlefieldIndex].myFormation[0]).toEqual(card);
-        expect(result.current.myHands.length).toBe(6);
+        it('フィールドが埋まっているとエラー', () => {
+          result.current.battlefields[0].myFormation = result.current.unitStack.slice(0, 3);
+          const test = () => act(() => result.current.playCard(0));
+          act(() => result.current.selectHand(0));
+          expect(test).toThrowError();
+          expect(result.current.myHands.length).toBe(7);
+        });
+      });
+
+      describe('士気高揚戦術カード', () => {
+        const alexander = {
+          type: 'TACTICAL',
+          id: TacticalType.ALEXANDER,
+          tacticalType: TacticalType.ALEXANDER,
+        } as const;
+        const darius = {
+          type: 'TACTICAL',
+          id: TacticalType.DARIUS,
+          tacticalType: TacticalType.DARIUS,
+        } as const;
+        const companionCavalry = {
+          type: 'TACTICAL',
+          id: TacticalType.COMPANION_CAVALRY,
+          tacticalType: TacticalType.COMPANION_CAVALRY,
+        } as const;
+        const shieldBearers = {
+          type: 'TACTICAL',
+          id: TacticalType.SHIELD_BEARERS,
+          tacticalType: TacticalType.SHIELD_BEARERS,
+        } as const;
+
+        beforeEach(() => {
+          result.current.myHands = [
+            { ...alexander },
+            { ...darius },
+            { ...companionCavalry },
+            { ...shieldBearers },
+            ...result.current.myHands.slice(4, 7),
+          ];
+        });
+
+        it('カードを場に出す', () => {
+          act(() => result.current.selectHand(0));
+          act(() => result.current.playCard(0));
+          expect(result.current.battlefields[0].myFormation[0]).toEqual(alexander);
+          expect(result.current.myHands).not.toContainEqual(alexander);
+          act(() => result.current.selectHand(0));
+          act(() => result.current.playCard(1));
+          expect(result.current.battlefields[1].myFormation[0]).toEqual(darius);
+          expect(result.current.myHands).not.toContainEqual(darius);
+          act(() => result.current.selectHand(0));
+          act(() => result.current.playCard(2));
+          expect(result.current.battlefields[2].myFormation[0]).toEqual(companionCavalry);
+          expect(result.current.myHands).not.toContainEqual(companionCavalry);
+          act(() => result.current.selectHand(0));
+          act(() => result.current.playCard(3));
+          expect(result.current.battlefields[3].myFormation[0]).toEqual(shieldBearers);
+          expect(result.current.myHands).not.toContainEqual(shieldBearers);
+        });
+
+        it('フィールドが埋まっているとエラー', () => {
+          result.current.battlefields[0].myFormation = result.current.unitStack.slice(0, 3);
+          const test = () => act(() => result.current.playCard(0));
+          act(() => result.current.selectHand(0));
+          expect(test).toThrowError();
+          expect(result.current.myHands).toContainEqual(alexander);
+          act(() => result.current.selectHand(1));
+          expect(test).toThrowError();
+          expect(result.current.myHands).toContainEqual(darius);
+          act(() => result.current.selectHand(2));
+          expect(test).toThrowError();
+          expect(result.current.myHands).toContainEqual(companionCavalry);
+          act(() => result.current.selectHand(3));
+          expect(test).toThrowError();
+          expect(result.current.myHands).toContainEqual(shieldBearers);
+        });
+      });
+
+      describe('気象戦術カード', () => {
+        const fog = {
+          type: 'TACTICAL',
+          id: TacticalType.FOG,
+          tacticalType: TacticalType.FOG,
+        } as const;
+        const mud = {
+          type: 'TACTICAL',
+          id: TacticalType.MUD,
+          tacticalType: TacticalType.MUD,
+        } as const;
+
+        beforeEach(() => {
+          result.current.myHands = [{ ...fog }, { ...mud }, ...result.current.myHands.slice(2, 7)];
+        });
+
+        it('カードを場に出す', () => {
+          act(() => result.current.selectHand(0));
+          act(() => result.current.playCard(0));
+          expect(result.current.battlefields[0].field[0]).toEqual(fog);
+          act(() => result.current.selectHand(0));
+          act(() => result.current.playCard(1));
+          expect(result.current.battlefields[1].field[0]).toEqual(mud);
+        });
       });
 
       it('カードを選択していないとエラー', () => {
@@ -72,7 +181,7 @@ describe('battleLine/store', () => {
         expect(() => act(() => result.current.playCard(battlefieldIndex))).toThrowError();
       });
 
-      it('境界テスト', () => {
+      it('存在しないフィールドに出すとエラー', () => {
         const test = (index: number) => () => act(() => result.current.playCard(index));
         act(() => result.current.selectHand(0));
         expect(test(-1)).toThrowError();
--- src/features/games/battleLine/store/playCard.ts
+++ src/features/games/battleLine/store/playCard.ts
@@ -1,4 +1,5 @@
 import { BattleLineStore } from '.';
+import { TacticalCard, TacticalType } from '../types';
 import { ZustandSet } from '@/libs/zustand';
 
 export const playCard =
@@ -8,15 +9,43 @@ export const playCard =
     if (index < 0 || index > 8) throw new Error('index out of range');
     return set((state) => {
       if (state.selectedIndex === null) throw new Error('no card selected');
-      const card = { ...state.myHands[state.selectedIndex] };
-      const newBattlefields = state.battlefields.map((b, i) =>
-        i === index ? { ...b, myFormation: [...b.myFormation, card] } : { ...b },
-      );
-      const newHands = state.myHands.filter((c) => c.id !== card.id);
-      return {
-        battlefields: newBattlefields,
-        myHands: newHands,
-        selectedIndex: null,
-      };
+      if (state.battlefields[index].myFormation.length >= 3) throw new Error('field is full');
+
+      switch (state.myHands[state.selectedIndex].id) {
+        case TacticalType.FOG:
+        case TacticalType.MUD:
+          return processWeatherCard(state, index);
+        default:
+          return processDefault(state, index);
+      }
     });
   };
+
+const processWeatherCard = (state: BattleLineStore, index: number) => {
+  const card = { ...state.myHands[state.selectedIndex!] } as TacticalCard;
+  const newBattlefields = state.battlefields.map((b, i) =>
+    i === index ? { ...b, field: [...b.field, card] } : { ...b },
+  );
+  const newHands = getNewHands(state);
+  return {
+    battlefields: newBattlefields,
+    myHands: newHands,
+    selectedIndex: null,
+  };
+};
+
+const processDefault = (state: BattleLineStore, index: number) => {
+  const card = { ...state.myHands[state.selectedIndex!] };
+  const newBattlefields = state.battlefields.map((b, i) =>
+    i === index ? { ...b, myFormation: [...b.myFormation, card] } : { ...b },
+  );
+  const newHands = getNewHands(state);
+  return {
+    battlefields: newBattlefields,
+    myHands: newHands,
+    selectedIndex: null,
+  };
+};
+
+const getNewHands = (state: BattleLineStore) =>
+  state.myHands.filter((_, i) => i !== state.selectedIndex);
