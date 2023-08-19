7d36b0aa4ccf7ffb93ac860f4c9a5caad53c2cc6
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-19 10:12:27 +0900

    :bento: add getCard

---
+++ src/features/games/battleLine/utils.ts
@@ -0,0 +1,34 @@
+import { TacticalCard, TacticalType, UnitCard, UnitColor, UnitValue } from './types';
+
+type UnitArgument = {
+  type: 'UNIT';
+  color: UnitColor;
+  value: UnitValue;
+};
+
+type TacticalArgument = {
+  type: 'TACTICAL';
+  tacticalType: TacticalType;
+};
+
+type Argument = UnitArgument | TacticalArgument;
+
+export function getCard(arg: UnitArgument): UnitCard;
+export function getCard(arg: TacticalArgument): TacticalCard;
+export function getCard(arg: Argument): UnitCard | TacticalCard {
+  switch (arg.type) {
+    case 'UNIT':
+      return {
+        type: 'UNIT',
+        id: `${arg.color}-${arg.value}`,
+        color: arg.color,
+        value: arg.value,
+      };
+    case 'TACTICAL':
+      return {
+        type: 'TACTICAL',
+        id: arg.tacticalType,
+        tacticalType: arg.tacticalType,
+      };
+  }
+}
