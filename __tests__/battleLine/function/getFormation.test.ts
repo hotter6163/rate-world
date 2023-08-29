7461d60142d39cad66eaa20019048bb2d2792e2a
yoshikazu hotta <hotta-nagoya-6163@au.com>
2023-08-29 15:58:29 +0900

    :recycle: functionのテストを別ファイルに分割

--- __tests__/battleLine/function/getFormation.test.ts
+++ __tests__/battleLine/function/getFormation.test.ts
@@ -1,7 +1,5 @@
-import { unitStack } from '@/features/games/battleLine/constants';
 import { getCard } from '@/features/games/battleLine/functions/getCard';
 import { getFormation } from '@/features/games/battleLine/functions/getFormation';
-import { getUnusedCards } from '@/features/games/battleLine/functions/getUnusedCards';
 import {
   Card,
   FormationType,
@@ -9,48 +7,6 @@ import {
   UnitCard,
   UnitColor,
 } from '@/features/games/battleLine/types';
-
-describe('getUnusedCards', () => {
-  const battlefields = [
-    {
-      myFormation: [
-        getCard({ type: 'UNIT', color: UnitColor.BLUE, value: 1 }),
-        getCard({ type: 'UNIT', color: UnitColor.BLUE, value: 2 }),
-      ],
-      opponentFormation: [getCard({ type: 'TACTICAL', tacticalType: TacticalType.ALEXANDER })],
-      field: [getCard({ type: 'TACTICAL', tacticalType: TacticalType.FOG })],
-    },
-  ];
-  const trash = {
-    mine: [getCard({ type: 'TACTICAL', tacticalType: TacticalType.SCOUT })],
-    opponent: [],
-  };
-  const usedCards = [
-    getCard({ type: 'UNIT', color: UnitColor.BLUE, value: 1 }),
-    getCard({ type: 'UNIT', color: UnitColor.BLUE, value: 2 }),
-    getCard({ type: 'TACTICAL', tacticalType: TacticalType.ALEXANDER }),
-    getCard({ type: 'TACTICAL', tacticalType: TacticalType.FOG }),
-    getCard({ type: 'TACTICAL', tacticalType: TacticalType.SCOUT }),
-  ];
-
-  it('使用されているカードは山札になく、使用されていないカードは山札に存在する', () => {
-    const { unitCards, tacticalCards } = getUnusedCards(battlefields, trash);
-    unitStack.forEach((card) => {
-      if (usedCards.some((usedCard) => usedCard.id === card.id)) {
-        expect(unitCards).not.toContain(card);
-      } else {
-        expect(unitCards).toContain(card);
-      }
-    });
-    tacticalCards.forEach((card) => {
-      if (usedCards.some((usedCard) => usedCard.id === card.id)) {
-        expect(tacticalCards).not.toContain(card);
-      } else {
-        expect(tacticalCards).toContain(card);
-      }
-    });
-  });
-});
 
 describe('getFormation', () => {
   const getTotal = (cards: UnitCard[]) => cards.reduce((acc, card) => acc + card.value, 0);
@@ -359,19 +315,19 @@ describe('getFormation', () => {
       });
     });
   });
-});
 
-it('必要な枚数カードがない場合はNONE', () => {
-  expect(
-    getFormation(
-      [
-        getCard({ type: 'UNIT', color: UnitColor.BLUE, value: 1 }),
-        getCard({ type: 'TACTICAL', tacticalType: TacticalType.ALEXANDER }),
-      ],
-      false,
-    ),
-  ).toEqual({
-    type: FormationType.NONE,
-    total: 11,
+  it('必要な枚数カードがない場合はNONE', () => {
+    expect(
+      getFormation(
+        [
+          getCard({ type: 'UNIT', color: UnitColor.BLUE, value: 1 }),
+          getCard({ type: 'TACTICAL', tacticalType: TacticalType.ALEXANDER }),
+        ],
+        false,
+      ),
+    ).toEqual({
+      type: FormationType.NONE,
+      total: 11,
+    });
   });
 });
